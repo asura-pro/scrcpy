c50beff52451233876b1b0f228a7ebf8537bc27e
xcorpio <ngxcorpio@gmail.com>
2021-05-25 10:20:20 +0800

    Send serial instead of device name

--- server/src/main/java/com/genymobile/scrcpy/DesktopConnection.java
+++ server/src/main/java/com/genymobile/scrcpy/DesktopConnection.java
@@ -3,12 +3,14 @@ package com.genymobile.scrcpy;
 import android.net.LocalServerSocket;
 import android.net.LocalSocket;
 import android.net.LocalSocketAddress;
+import android.os.Build;
 
 import java.io.Closeable;
 import java.io.FileDescriptor;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.lang.reflect.Method;
 import java.nio.charset.StandardCharsets;
 
 public final class DesktopConnection implements Closeable {
@@ -71,7 +73,11 @@ public final class DesktopConnection implements Closeable {
 
         DesktopConnection connection = new DesktopConnection(videoSocket, controlSocket);
         Size videoSize = device.getScreenInfo().getVideoSize();
-        connection.send(Device.getDeviceName(), videoSize.getWidth(), videoSize.getHeight());
+        // connection.send(Device.getDeviceName(), videoSize.getWidth(), videoSize.getHeight());
+        String serial = getSerialNumber();
+        Ln.i("Serial: " + serial);
+        connection.send("V:" + serial, videoSize.getWidth(), videoSize.getHeight());
+        connection.sendControl("C:" + serial, videoSize.getWidth(), videoSize.getHeight());
         return connection;
     }
 
@@ -97,6 +103,63 @@ public final class DesktopConnection implements Closeable {
         buffer[DEVICE_NAME_FIELD_LENGTH + 2] = (byte) (height >> 8);
         buffer[DEVICE_NAME_FIELD_LENGTH + 3] = (byte) height;
         IO.writeFully(videoFd, buffer, 0, buffer.length);
+    }
+
+    private void sendControl(String deviceName, int width, int height) throws IOException {
+        byte[] buffer = new byte[DEVICE_NAME_FIELD_LENGTH + 4];
+
+        byte[] deviceNameBytes = deviceName.getBytes(StandardCharsets.UTF_8);
+        int len = StringUtils.getUtf8TruncationIndex(deviceNameBytes, DEVICE_NAME_FIELD_LENGTH - 1);
+        System.arraycopy(deviceNameBytes, 0, buffer, 0, len);
+        // byte[] are always 0-initialized in java, no need to set '\0' explicitly
+
+        buffer[DEVICE_NAME_FIELD_LENGTH] = (byte) (width >> 8);
+        buffer[DEVICE_NAME_FIELD_LENGTH + 1] = (byte) width;
+        buffer[DEVICE_NAME_FIELD_LENGTH + 2] = (byte) (height >> 8);
+        buffer[DEVICE_NAME_FIELD_LENGTH + 3] = (byte) height;
+        controlOutputStream.write(buffer);
+        controlOutputStream.flush();
+    }
+
+    public static String getSerialNumber() {
+        String serialNumber;
+        try {
+            Class<?> c = Class.forName("android.os.SystemProperties");
+            Method get = c.getMethod("get", String.class);
+            // (?) Lenovo Tab (https://stackoverflow.com/a/34819027/1276306)
+            serialNumber = (String) get.invoke(c, "gsm.sn1");
+            if (serialNumber.equals(""))
+                // Samsung Galaxy S5 (SM-G900F) : 6.0.1
+                // Samsung Galaxy S6 (SM-G920F) : 7.0
+                // Samsung Galaxy Tab 4 (SM-T530) : 5.0.2
+                // (?) Samsung Galaxy Tab 2 (https://gist.github.com/jgold6/f46b1c049a1ee94fdb52)
+                serialNumber = (String) get.invoke(c, "ril.serialnumber");
+            if (serialNumber.equals(""))
+                // Archos 133 Oxygen : 6.0.1
+                // Google Nexus 5 : 6.0.1
+                // Hannspree HANNSPAD 13.3" TITAN 2 (HSG1351) : 5.1.1
+                // Honor 5C (NEM-L51) : 7.0
+                // Honor 5X (KIW-L21) : 6.0.1
+                // Huawei M2 (M2-801w) : 5.1.1
+                // (?) HTC Nexus One : 2.3.4 (https://gist.github.com/tetsu-koba/992373)
+                serialNumber = (String) get.invoke(c, "ro.serialno");
+            if (serialNumber.equals(""))
+                // (?) Samsung Galaxy Tab 3 (https://stackoverflow.com/a/27274950/1276306)
+                serialNumber = (String) get.invoke(c, "sys.serialnumber");
+            if (serialNumber.equals(""))
+                // Archos 133 Oxygen : 6.0.1
+                // Hannspree HANNSPAD 13.3" TITAN 2 (HSG1351) : 5.1.1
+                // Honor 9 Lite (LLD-L31) : 8.0
+                // Xiaomi Mi 8 (M1803E1A) : 8.1.0
+                serialNumber = Build.SERIAL;
+            // If none of the methods above worked
+            if (serialNumber.equals(Build.UNKNOWN))
+                serialNumber = Device.getDeviceName();
+        } catch (Exception e) {
+            e.printStackTrace();
+            serialNumber = Device.getDeviceName();
+        }
+        return serialNumber;
     }
 
     public FileDescriptor getVideoFd() {
